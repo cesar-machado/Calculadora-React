887cd26b388244216b3813fb6e035372d698faf8
cesar-machado <cesarmach19@gmail.com>
2020-03-22 00:47:15 -0300

    Terminei o projeto, coloquei a logica e esta funcionando.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Projeto Calculadora (Aprendendo React)
+
+Treinando React e criando um projeto de calculadora para evoluir no react.
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''    
+    classes += props.double ? 'double' : ''    
+    classes += props.triple ? 'triple' : ''    
+    
+    return (
+        <button 
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label}
+        </button>
 
-export default props => 
-<button className="button">{props.label}</button>
-
-
-
-
+    )
+}
+    
+    
+    
+    
+    
